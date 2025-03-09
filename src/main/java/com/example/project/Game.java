2513dfad9be24f8eeb2d72e00d4f71cd6518053b
jacobw058 <jacobw96@nycstudents.net>
2025-03-09 07:35:07 +0000

    final push

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -3,14 +3,158 @@ import java.util.ArrayList;
 
 
 public class Game{
-    public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
-        return "Error";
+    //static method to determine the winner in a poker game with players p1 and p2, with hands of p1Hand and p2Hand, and community cards
+    // represented by the ArrayList communityCards
+    public static String determineWinner(Player p1, Player p2, String p1Hand, String p2Hand, ArrayList<Card> communityCards){
+        int numWin = getOneOrTwoGreater(Utility.getHandRanking(p1Hand), Utility.getHandRanking(p2Hand));
+        if (numWin == 1) {
+            return "Player 1 wins!";
+        } else if (numWin == 2) {
+            return "Player 2 wins!";
+        } else {
+            if (Utility.getHandRanking(p1Hand) == 1) {
+                return greaterHighCard(p1, p2);
+            } else if (Utility.getHandRanking(p1Hand) == 2) {
+                return greaterHighCard(p1, p2);
+            } else {
+                return greaterMultiple(p1, p2, communityCards);
+            }
+        }
+    }
+
+    //static utility method to determine the highest ranking between two players p1 and p2 with the communityCards ArrayList
+    //this logic solves for highest pair, twopair, three of a kind, or full house
+    public static String greaterMultiple(Player p1, Player p2, ArrayList<Card> communityCards) {
+        String type = "";
+        int count = 0;
+        int pairs = 0;
+        String p1Pair = "";
+        String p1Pair2 = "";
+        String p1ThreeOfAKind = "";
+        for (int i = 0; i < p1.getAllCards().size(); i++) {
+            for (int j = 0; j < p1.getAllCards().size(); j++) {
+                if (Utility.getRankValue((p1.getAllCards().get(i).getRank())) == Utility.getRankValue((p1.getAllCards().get(j).getRank()))) {
+                    count++;
+                }
+                if (count == 2) {
+                    if (pairs == 1) {
+                        type = "twopair";
+                        p1Pair2 = p1.getAllCards().get(i).getRank();
+                    }
+                    p1Pair = p1.getAllCards().get(i).getRank();
+                    pairs++;
+                }
+                if (count == 3) {
+                    p1ThreeOfAKind = p1.getAllCards().get(i).getRank();
+                }
+            }
+        }
+        if (!p1Pair.equals("")) {
+            type = "pair";
+        }
+        if (!p1ThreeOfAKind.equals("")) {
+            type = "three";
+        }
+        if (!p1Pair.equals("") && !p1ThreeOfAKind.equals("")) {
+            type = "fullhouse";
+        }
+        count = 0;
+        pairs = 0;
+        String p2Pair = "";
+        String p2Pair2 = "";
+        String p2ThreeOfAKind = "";
+        for (int i = 0; i < p2.getAllCards().size(); i++) {
+            for (int j = 0; j < p2.getAllCards().size(); j++) {
+                if (Utility.getRankValue((p2.getAllCards().get(i).getRank())) == Utility.getRankValue((p2.getAllCards().get(j).getRank()))) {
+                    count++;
+                }
+                if (count == 2) {
+                    if (pairs == 1) {
+                        p2Pair2 = p2.getAllCards().get(i).getRank();
+                    }
+                    p2Pair = p2.getAllCards().get(i).getRank();
+                    pairs++;
+                }
+                if (count == 3) {
+                    p2ThreeOfAKind = p2.getAllCards().get(i).getRank();
+                }
+            }
+        }
+        int p1HighPairRank = Utility.getRankValue(p1Pair);
+        int p2HighPairRank = Utility.getRankValue(p2Pair);
+        int p1HighThreeRank = Utility.getRankValue(p1ThreeOfAKind);
+        int p2HighThreeRank = Utility.getRankValue(p2ThreeOfAKind);
+        if (type.equals("pair")) {
+            if (getOneOrTwoGreater(p1HighPairRank, p2HighPairRank) == 1) {
+                return "Player 1 wins!";
+            } else if (getOneOrTwoGreater(p1HighPairRank, p2HighPairRank) == 2) {
+                return "Player 2 wins!";
+            } else {
+                return greaterHighCard(p1, p2);
+            }
+        } else if (type.equals("three") || type.equals("fullhouse")) {
+            if (getOneOrTwoGreater(p1HighThreeRank, p2HighThreeRank) == 1) {
+                return "Player 1 wins!";
+            } else if (getOneOrTwoGreater(p1HighThreeRank, p2HighThreeRank) == 2) {
+                return "Player 2 wins!";
+            } else {
+                return greaterHighCard(p1, p2);
+            }
+        } else {
+            int p1Max = getGreater(Utility.getRankValue(p1Pair), Utility.getRankValue(p1Pair2));
+            int p2Max = getGreater(Utility.getRankValue(p2Pair), Utility.getRankValue(p2Pair2));
+            if (getOneOrTwoGreater(p1Max, p2Max) == 1) {
+                return "Player 1 wins!";
+            } else if (getOneOrTwoGreater(p1Max, p2Max) == 2) {
+                return "Player 2 wins!";
+            } else {
+                return greaterHighCard(p1, p2);
+            }
+        }
+    }
+
+    //logic to determine the winner between two players p1 and p2 based only on which player
+    //has the highest high card in hand
+    public static String greaterHighCard(Player p1, Player p2) {
+        int p1HighCard = getGreater(Utility.getRankValue(p1.getHand().get(0).getRank()), Utility.getRankValue(p1.getHand().get(1).getRank()));
+        int p2HighCard = getGreater(Utility.getRankValue(p2.getHand().get(0).getRank()), Utility.getRankValue(p2.getHand().get(1).getRank()));
+        if (p1HighCard == p2HighCard) {
+            return "Tie!";
+        } else if (p1HighCard > p2HighCard) {
+            return "Player 1 wins!";
+        } else {
+            return "Player 2 wins!";
+        }
+    }
+
+    //utility method for determining the greater number between two int parameters num1 and num2
+    //returns a placeholder value for cases where neither of the inputted values should be returned
+    public static int getOneOrTwoGreater(int num1, int num2) {
+        if (num1 > num2) {
+            return 1;
+        } else if (num2 > num1) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+
+    //utility method for determining the greater number between two int parameters num1 and num2
+    //returns the greater of the two, if both are equal it returns that value
+    public static int getGreater(int num1, int num2) {
+        int result = getOneOrTwoGreater(num1, num2);
+        if (result == -1) {
+            return num1;
+        } else if (result == 1) {
+            return num1;
+        }  else {
+            return num2;
+        }
     }
 
     public static void play(){ //simulate card playing
     
     }
         
-        
+}       
 
-}
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -5,35 +5,185 @@ import java.util.ArrayList;
 public class Player{
     private ArrayList<Card> hand;
     private ArrayList<Card> allCards; //the current community cards + hand
-    String[] suits  = Utility.getSuits();
+    String[] suits = Utility.getSuits();
     String[] ranks = Utility.getRanks();
     
-    public Player(){
+    public Player(){ //Player constructor to construct a hand ArrayList
         hand = new ArrayList<>();
     }
-
+    //getter method for the player's hand
     public ArrayList<Card> getHand(){return hand;}
+    //getter method for the player's hand and the community cards
     public ArrayList<Card> getAllCards(){return allCards;}
-
+    //void method to add a card to the player's hand
     public void addCard(Card c){
-        
+        hand.add(c);
+    }
+    //method to simulate playing a poker hand with an ArrayList parameter communityCards
+    public String playHand(ArrayList<Card> communityCards){      
+        allCards = new ArrayList<Card>();
+        for (int i = 0; i < communityCards.size(); i++) {
+            allCards.add(communityCards.get(i));
+        }
+        for (int j = 0; j < hand.size(); j++) {
+            allCards.add(hand.get(j));
+        }
+        if (checkFlushStraight(allCards) != "Nothing") {
+            return checkFlushStraight(allCards);
+        } else if (checkMultiples(allCards) != "Nothing") {
+            return checkMultiples(allCards);
+        } else if (checkHighCard(communityCards, hand) != "Nothing") {
+            return checkHighCard(communityCards, hand);
+        } else {
+            return "Nothing";
+        }
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    //method to check if a player has a higher rank card in hand than the highest in the communityCards card pool
+    public String checkHighCard(ArrayList<Card> communityCards, ArrayList<Card> handCards) {
+        int comMax = 0;
+        int handMax = 0;
+        for (Card card : communityCards) {
+            if (Utility.getRankValue(card.getRank()) > comMax) {
+                comMax = Utility.getRankValue(card.getRank());
+            }
+        }
+        for (Card card : handCards) {
+            if (Utility.getRankValue(card.getRank()) > handMax) {
+                handMax = Utility.getRankValue(card.getRank());
+            }
+        }
+        if (handMax > comMax) {
+            return "High Card";
+        }
         return "Nothing";
     }
 
-    public void sortAllCards(){} 
+    //method to check if a player has a flush, straight, straight flush, or royal flush
+    public String checkFlushStraight(ArrayList<Card> cards) {
+        boolean flush = false;
+        for (int i = 0; i < 4; i++) {
+            int count = 0;
+            for (int j = 0; j < cards.size(); j++) {
+                if (cards.get(j).getSuit() == suits[i]) {
+                    count++;
+                }
+            }
+            if (count == 5) {
+                flush = true;
+            }
+        }
+        boolean straight = false;
+        boolean royal = false;
+        ArrayList<Integer> ranksList = new ArrayList<Integer>();
+        for (int q = 0; q < cards.size(); q++) {
+            ranksList.add(Utility.getRankValue(cards.get(q).getRank()));
+        }
+        for (int x = 2; x < 11; x++) {
+            if (ranksList.indexOf(x) > -1 && ranksList.indexOf(x + 1) > -1 && ranksList.indexOf(x + 2) > -1
+             && ranksList.indexOf(x + 3) > -1 && ranksList.indexOf(x + 4) > -1) {
+                if (x == 10) {
+                    royal = true;
+                }
+                straight = true;
+            }
+        }
+        if (flush && royal) {
+            return "Royal Flush";
+        } else if (flush && straight) {
+            return "Straight Flush";
+        } else if (flush) {
+            return "Flush";
+        } else if (straight) {
+            return "Straight";
+        } else {
+            return "Nothing";
+        }
+    }
 
+    //method to check if a player has a pair, twopair, three of a kind, or a full house
+    public String checkMultiples(ArrayList<Card> cards) {
+        int alt = 0;
+        String used = "";
+        for (int i = 0; i < cards.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < cards.size(); j++) {
+                if (cards.get(i).getRank() == cards.get(j).getRank() && cards.get(i).getRank() != used) {
+                    count++;
+                }
+            }
+            used = cards.get(i).getRank();
+            if (count >= 2) {
+                if (count == 4) {
+                    return "Four of a Kind";
+                }
+                if (count + alt == 4) {
+                    return "Two Pair";
+                }
+                if (count + alt == 5) {
+                    return "Full House";
+                }
+                if (count >= 2) {
+                    alt = count;
+                }
+            }
+            if (i == cards.size() - 1) {
+                if (alt == 3) {
+                    return "Three of a Kind";
+                }
+                if (alt == 2) {
+                    return "A Pair";
+                }
+            }
+        }
+        return "Nothing";
+    }
+
+    //method to sort cards by rank value order
+    public void sortAllCards() {
+        for (int i = 1; i < allCards.size(); i++) {
+            Card key = allCards.get(i);
+            int keyRank = Utility.getRankValue(key.getRank());
+            int j = i - 1;
+            while (j >= 0 && Utility.getRankValue(allCards.get(j).getRank()) > keyRank) {
+                allCards.set(j + 1, allCards.get(j));
+                j--;
+            }
+            allCards.set(j + 1, key);
+        }
+    }
+
+    //method to find the frequency of each rank, returns an ArrayList with the frequencies in order
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freq = new ArrayList<Integer>(); 
+        for (int i = 0; i < 13; i++) {
+            int count = 0;
+            for (int j = 0; j <allCards.size(); j++) {
+                if (allCards.get(j).getRank() == ranks[i]) {
+                    count++;
+                }
+            }
+            freq.add(count);
+        }
+        return freq;
     }
 
-    public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+    //method to find the frequency of each suit, returns an ArrayList with the suits in order
+    public ArrayList<Integer> findSuitFrequency(){ // spades, hearts, clubs, diamonds
+        ArrayList<Integer> freq = new ArrayList<Integer>();
+        for (int i = 0; i < 4; i++) {
+            int count = 0;
+            for (int j = 0; j < allCards.size(); j++) {
+                if (allCards.get(j).getSuit() == suits[i]) {
+                    count++;
+                }
+            }
+            freq.add(count);
+        }
+        return freq;
     }
 
-   
+   //toString method
     @Override
     public String toString(){
         return hand.toString();
